22707e9db94994cedaffb4bfc3207e9c61f8032f
CarlosLeonGaleas <jostinleon18@gmail.com>
2025-07-16 20:23:39 +0000

    Se agregaron los estados para el manejo de las 5 preguntas del cuestionario. Se agregó el componente para mostrar las preguntas en pantalla. Se debe gestionar la actualización de las preguntas desde la respuesta de NFC.

--- app/src/main/java/com/example/myndef/ui/components/MainScreen.kt
+++ app/src/main/java/com/example/myndef/ui/components/MainScreen.kt
@@ -9,27 +9,45 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.selection.selectable
+import androidx.compose.foundation.selection.selectableGroup
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.RadioButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.semantics.Role
 import com.example.myndef.R
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.lifecycle.viewmodel.compose.viewModel
 
 @Composable
 fun MainScreen(
     name: String,
     phone: String,
     apduCommand: String,
-    onLogout: () -> Unit
+    onLogout: () -> Unit,
+    viewModel: MainScreenViewModel = viewModel()
 ) {
+    // Estados observables del ViewModel
+    val q1 by viewModel.q1.collectAsState()
+    val q2 by viewModel.q2.collectAsState()
+    val q3 by viewModel.q3.collectAsState()
+    val q4 by viewModel.q4.collectAsState()
+    val q5 by viewModel.q5.collectAsState()
+
     Column(
         modifier = Modifier
             .fillMaxSize()
@@ -42,6 +60,12 @@ fun MainScreen(
         Text("Acerque su teléfono para acceder al cuestionario")
 
         ApduCard(apduCommand)
+
+        OptionsQuestion(question = q1)
+        OptionsQuestion(question = q2)
+        OptionsQuestion(question = q3)
+        OptionsQuestion(question = q4)
+        OptionsQuestion(question = q5)
     }
 }
 
@@ -83,6 +107,44 @@ fun ApduCard(command: String) {
             Text("APDU Command:")
             Spacer(modifier = Modifier.height(8.dp))
             Text(text = command, fontSize = 12.sp)
+        }
+    }
+}
+
+@Composable
+fun OptionsQuestion(
+    modifier: Modifier = Modifier,
+    question: String,
+) {
+    val questionSplited = question.split("|")
+    val radioOptions = listOf(questionSplited[1], questionSplited[2], questionSplited[3], questionSplited[4])
+    val (selectedOption, onOptionSelected) = remember { mutableStateOf(radioOptions[0]) }
+    // Note that Modifier.selectableGroup() is essential to ensure correct accessibility behavior
+    Column(modifier.selectableGroup()) {
+        Text(questionSplited[0])
+        radioOptions.forEach { text ->
+            Row(
+                Modifier
+                    .fillMaxWidth()
+                    .height(56.dp)
+                    .selectable(
+                        selected = (text == selectedOption),
+                        onClick = { onOptionSelected(text) },
+                        role = Role.RadioButton
+                    )
+                    .padding(horizontal = 16.dp),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                RadioButton(
+                    selected = (text == selectedOption),
+                    onClick = null // null recommended for accessibility with screen readers
+                )
+                Text(
+                    text = text,
+                    style = MaterialTheme.typography.bodyLarge,
+                    modifier = Modifier.padding(start = 16.dp)
+                )
+            }
         }
     }
 }
---
+++ app/src/main/java/com/example/myndef/ui/components/MainScreenViewModel.kt
@@ -0,0 +1,52 @@
+package com.example.myndef.ui.components
+
+import androidx.lifecycle.ViewModel
+import com.example.myndef.MainActivityViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+
+class MainScreenViewModel : ViewModel(){
+    // Estados privados mutables
+    private val _q1 = MutableStateFlow("¿Cuál es la capital del Ecuador?|Guayaquil|Cuenca|Quito|Ambato|2")
+    private val _q2 = MutableStateFlow("¿Cuál es la capital del Ecuador?|Quito|Ambato|Guayaquil|Cuenca|2")
+    private val _q3 = MutableStateFlow("¿Cuál es la capital del Ecuador?|Guayaquil|Cuenca|Quito|Ambato|2")
+    private val _q4 = MutableStateFlow("¿Cuál es la capital del Ecuador?|Cuenca|Quito|Guayaquil|Ambato|2")
+    private val _q5 = MutableStateFlow("¿Cuál es la capital del Ecuador?|Quito|Guayaquil|Cuenca|Ambato|2")
+
+    // Estados públicos inmutables
+    val q1: StateFlow<String> = _q1.asStateFlow()
+    val q2: StateFlow<String> = _q2.asStateFlow()
+    val q3: StateFlow<String> = _q3.asStateFlow()
+    val q4: StateFlow<String> = _q4.asStateFlow()
+    val q5: StateFlow<String> = _q5.asStateFlow()
+
+    // Instancia estática para acceso desde el BroadcastReceiver
+    companion object {
+        var instance: MainScreenViewModel? = null
+    }
+
+    init {
+        instance = this
+    }
+
+    fun updateQ1(question: String){
+        _q1.value = question
+    }
+
+    fun updateQ2(question: String){
+        _q2.value = question
+    }
+
+    fun updateQ3(question: String){
+        _q3.value = question
+    }
+
+    fun updateQ4(question: String){
+        _q4.value = question
+    }
+
+    fun updateQ5(question: String){
+        _q5.value = question
+    }
+}
